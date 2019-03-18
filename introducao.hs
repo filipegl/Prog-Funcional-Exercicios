7ec86aaedcaf7c321ff1dc3cd13523ce3a5ea72e
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2019-03-18 15:50:53 -0300

    Incompleto

---
+++ introducao.hs
@@ -0,0 +1,71 @@
+{-
+- Usando os predicados not,and e or prontos de Haskell, implemente os predicados (funcoes) xor (or exclusivo),
+- impl (implicacao A => B é equivalente a (not A or B)) e equiv (A <=> B é definido como A => B and B => A)
+- Procure usar casamento de padroes e reutilizar as funcoes.
+-}
+xor p q = (p && (not q)) || (q && (not p))
+impl a b = (not a) || b
+equiv a b = ((not a) || b) && ((not b) || a)
+
+{-
+A funcao square esta implementada e eleva ao quadrado um determinado numero
+-}
+square x = x*x
+
+{-
+- Implemente a funcao potencia, que retorna o resultado de x elevado a y 
+-}
+pow x y = x**y
+
+
+{-
+- Implemente a funcao fatorial que calcula o fatorial de um numero 
+-}
+fatorial 0 = 1
+fatorial x = x * fatorial(x-1)
+
+{-
+- Determina se um numero eh primo ou nao. Preocupe-se apenas em resolver o problema.
+- Nao precisa usar conhecimentos mais sofisticados da teoria dos numeros. Voce pode trabalhar com listas.
+-}
+factors n = [x | x <- [1..n], (mod n x) == 0]
+isPrime n = factors n == [1,n]
+
+{-
+- Calcula um termo da sequencia de Fibonnacci. Voce pode trabalhar com listas. 
+-}
+-- 1, 1, 2, 3, 5, 8, 13, 21...
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+{-
+- Calcula um MDC de dois numeros usando o algoritmo de Euclides. 
+-}
+mdc a 0 = a
+mdc a b = mdc b (mod a b)
+
+
+--TODO
+--mdc a 0 = a
+--mdc 0 b = b
+--mdc a a = a
+--mdc a b
+--    |a > b = mdc (a-b) b
+--    |b > a = mdc (b - a) a
+
+{-
+- Calcula um MMC de dois numeros. 
+-}
+mmc x y = undefined
+
+{-
+- Determina se dois numeros inteiros positivos sao co-primos. Dois numeros sao co-primos se 
+- o mdc deles for igual a 1. Ex: coprimo 35 64 = True 
+-}
+coprimo x y = undefined
+
+{-
+- Calcula a conjectura de Goldbach, que diz que um numero par maior que 2 pode ser escrito como a soma de dois numeros primos. Ex: 28 = 5 + 23.
+-}
+goldbach x = undefined
