5395847df40344c4c73eb7aa6112803db10a82ff
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2019-03-19 16:59:39 -0300

    Completo

--- introducao.hs
+++ introducao.hs
@@ -1,6 +1,6 @@
 {-
 - Usando os predicados not,and e or prontos de Haskell, implemente os predicados (funcoes) xor (or exclusivo),
-- impl (implicacao A => B Ã© equivalente a (not A or B)) e equiv (A <=> B Ã© definido como A => B and B => A)
+- impl (implicacao A => B é equivalente a (not A or B)) e equiv (A <=> B é definido como A => B and B => A)
 - Procure usar casamento de padroes e reutilizar as funcoes.
 -}
 xor p q = (p && (not q)) || (q && (not p))
@@ -16,7 +16,6 @@ square x = x*x
 - Implemente a funcao potencia, que retorna o resultado de x elevado a y 
 -}
 pow x y = x**y
-
 
 {-
 - Implemente a funcao fatorial que calcula o fatorial de um numero 
@@ -45,27 +44,25 @@ fib n = fib (n-1) + fib (n-2)
 mdc a 0 = a
 mdc a b = mdc b (mod a b)
 
-
---TODO
---mdc a 0 = a
---mdc 0 b = b
---mdc a a = a
---mdc a b
---    |a > b = mdc (a-b) b
---    |b > a = mdc (b - a) a
-
 {-
-- Calcula um MMC de dois numeros. 
+- Calcula um MMC de dois numeros.
+- Resposta: A funcao filter ira retornar uma nova lista filtrada: A lista de todos os multiplos comuns. 
+- Os elementos `n` dessa lista quando aplicados na funcao `divisivel x y n`, retornarao `True`.
+- O mmc sera o primeiro elemento dessa lista. 
 -}
-mmc x y = undefined
+mmc x y = head multiplos_comuns
+  where multiplos_comuns = filter (divisivel x y) [(min x y)..x*y]
+divisivel x y n = (mod n x == 0) && (mod n y == 0)
 
 {-
 - Determina se dois numeros inteiros positivos sao co-primos. Dois numeros sao co-primos se 
 - o mdc deles for igual a 1. Ex: coprimo 35 64 = True 
 -}
-coprimo x y = undefined
+coprimo x y
+    |(mdc x y) == 1 = True
+    |otherwise = False
 
 {-
 - Calcula a conjectura de Goldbach, que diz que um numero par maior que 2 pode ser escrito como a soma de dois numeros primos. Ex: 28 = 5 + 23.
 -}
-goldbach x = undefined
+goldbach x = [(a,b) | a <- filter isPrime [2..x-1], b <- filter isPrime [2..x-1], a+b == x]
