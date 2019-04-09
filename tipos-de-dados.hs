93a8a31f2294d1745d152f42bce33e38b81c51f9
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2019-04-08 22:55:30 -0300

    Falta implementar: isBST, insert, remove, preOrder, order e postOrder

--- tipos-de-dados.hs
+++ tipos-de-dados.hs
@@ -62,6 +62,7 @@ sizeBST (Node a left right) = 1 + sizeBST left + sizeBST right
 
 --verifica se uma BT é uma BST
 isBST NIL = True
+--TODO
 --isBST (Node a left right)
 --	| a < left = (isBST left) && (isBST right)
 --	| otherwise = False
@@ -70,19 +71,59 @@ isBST NIL = True
 insert = undefined
 
 --retorna o Node da BST contendo o dado procurado ou entao NIL
-search = undefined
+search element (Node a left right)
+    | (element == a) = (Node a left right)
+    | ((left == NIL) && (right == NIL)) = NIL
+    | (element > a) = search element right
+    | (element < a) = search element left
 
 --retorna o elmento maximo da BST
-maximum = undefined
+maxim (Node a left right)
+    | (right /= NIL) = maxim right
+    | otherwise = a
 
 --retorna o elemento minimo da BST
-minimum = undefined
+minim (Node a left right)
+    | (left /= NIL) = minim left
+    | otherwise = a
 
+-- algoritimo (java) em: https://www.techiedelight.com/find-inorder-predecessor-given-key-bst/
+getParentPredecessor element prec (Node a left right)
+    | (root == NIL) = prec
+    | (element == a) = if (left /= NIL) then maxim left else prec
+    | (element < a) = getParentPredecessor element prec left
+    | otherwise = getParentPredecessor element a right
+    where root = (Node a left right) 
 --retorna o predecessor de um elemento da BST, caso o elemento esteja na BST
-predecessor = undefined
+-- chamada: predecessor elemento bst
+predecessor element (Node a left right)
+    | (node == NIL) = error "Not Found"
+    | (element == (minim node)) = error "Has not predecessor"
+    | otherwise = getParentPredecessor element a (Node a left right)
+    where node = search element (Node a left right)
+
 
 --retorna o sucessor de um elemento da BST, caso o elemento esteja na BST
-successor = undefined
+-- obs: o codigo `(\(Just x) -> x) rightElem` e um converor do tipo Maybe.
+successor element (Node a left right)
+    | (node == NIL) = error "Not Found"
+    | (rightElem == Nothing) = error "This node is a leaf"
+    | otherwise = (\(Just x) -> x) rightElem
+    where node = search element (Node a left right)
+          rightElem = getRight node
+
+getElement NIL = Nothing
+getElement (Node a left right) = (Just a)
+
+getLeft (Node a left right) = getElement left
+
+getRight (Node a left right) = getElement right
+
+--`isParent element bst` retorna True se a bst for nó pai do elemento.
+isParent element (Node a left right)
+    | ((getElement left) == element) = True
+    | ((getElement right) == element) = True
+    | otherwise = False
 
 --remove ume lemento da BST
 remove = undefined
@@ -91,3 +132,16 @@ remove = undefined
 preOrder = undefined
 order = undefined
 postOrder = undefined
+
+-- arvore representada nesta imagem: 
+-- https://cdncontribute.geeksforgeeks.org/wp-content/uploads/BST.png
+vinteecinco = (Node 25 NIL NIL)
+dezenove = (Node 19 NIL NIL)
+vinteeum = (Node 21 dezenove vinteecinco)
+doze = (Node 12 nove vinteeum)
+nove = (Node 9 NIL NIL)
+tres = (Node 3 NIL NIL)
+um = (Node 1 NIL NIL)
+dois = (Node 2 um tres)
+cinco = (Node 5 dois doze) -- raiz
+-- exemplo de operacao: predecessor 19 cinco = 12
