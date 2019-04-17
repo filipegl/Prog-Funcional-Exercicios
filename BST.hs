2f8a28b6667ea68a25f21aee9cf369d0b6b2f79e
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2019-04-17 17:09:45 -0300

    Exercício sobre módulos: modularizar todo o código da BST implementada como um tipo de dado no exercício anterior. Feito

--- BST.hs
+++ BST.hs
@@ -1,58 +1,18 @@
---Escreva a declaracao para o tipo Triple, contendo tres elementos, todos de tipos diferentes.
---Escreva funcoes tripleFst, tripleSnd, tripleThr para extrair respectivamente o primeiro, segundo e terceiro
--- elementos de uma triple.
-data Triple a b c = Triple a b c deriving (Eq,Show)
-
-tripleFst (Triple a b c) = a
-tripleSnd (Triple a b c) = b
-tripleThr (Triple a b c) = c
-
---Escreva um tipo Quadruple que contem 4 elementos: dois de um mesmo tipo e outros dois de outro tipo
---Escreva as funcoes frstTwo e secondTwo que retornam os dois primeiros e os dois ultimos, respectivamente
-data Quadruple a b = Quadruple a a b b deriving (Eq,Show)
-
-firstTwo (Quadruple a b c d) = (a, b)
-secondTwo (Quadruple a b c d) = (c, d)
-
---Escreva um tipo de dados que pode conter um, dois, tres ou quatro elementos, dependendo do construtor
---Implemente funções tuple1 até tuple4 que que retornam Just <valor> ou Nothing se o valor nao existe
-data Tuple a b c d = Tuple1 a | Tuple2 a b | Tuple3 a b c | Tuple4 a b c d deriving (Eq,Show)
-
-tuple1 (Tuple1 a) = Just a
-tuple1 (Tuple2 a b) = Just a
-tuple1 (Tuple3 a b c) = Just a
-tuple1 (Tuple4 a b c d) = Just a
-
-tuple2 (Tuple2 a b)= Just b
-tuple2 (Tuple3 a b c)= Just b
-tuple2 (Tuple4 a b c d)= Just b 
-tuple2 _ = Nothing
-
-tuple3 (Tuple3 a b c) = Just c
-tuple3 (Tuple4 a b c d) = Just c 
-tuple3 _ = Nothing
-
-tuple4 (Tuple4 a b c d) = Just d
-tuple4 _ = Nothing
-
-data List a = Nil | Cons a (List a) deriving (Eq,Show)
-
-listLength Nil = 0
-listLength (Cons x xs) = 1 + listLength xs
-
-listHead Nil = error "Empty list"
-listHead (Cons x xs) = x
-
-listTail Nil = error "Empty list"
-listTail (Cons x xs) = xs
-
-listFoldr f v Nil = v
-listFoldr f v (Cons x xs) = f x (listFoldr f v xs)
-
-
-listFoldl f v Nil = v
-listFoldl f v (Cons x xs) = listFoldl f (f v x) xs 
-
+module BST (
+        BinaryTree (..),
+        sizeBST,
+        isBST,
+        search,
+        maxim,
+        minim,
+        predecessor,
+        successor,
+        remove,
+        preOrder,
+        order,
+        postOrder
+)
+where
 --Escreva as funcoes sobre a estrutura de dados binary tree
 data BinaryTree a = NIL | Node a (BinaryTree a) (BinaryTree a)
  deriving (Eq,Show)
---
+++ lerBST.hs
@@ -0,0 +1,5 @@
+-- ao fazer `ghci lerBST.sh` pode usar os metodos do arquivo BST.sh
+module Ler
+where
+
+import BST
